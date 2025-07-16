39e47e8a4cdc9673713860031d12fe6778aed1e9
Real1236 <arthurchan2001@live.com>
2025-07-16 11:27:24 -0400

    created TabScreen class

--- lib/navigation/main_nav.dart
+++ lib/navigation/main_nav.dart
@@ -8,6 +8,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+enum AppTab { timeline, snap, calendar }
+
 class MainNav extends StatefulWidget {
   const MainNav({super.key, required this.cameras});
   final List<CameraDescription> cameras;
@@ -17,66 +19,86 @@ class MainNav extends StatefulWidget {
 }
 
 class _MainNavState extends State<MainNav> {
-  int _current = 0;
-  final GlobalKey<GratitudeSnapScreenState> _snapKey =
-      GlobalKey<GratitudeSnapScreenState>();
-  final GlobalKey<CalendarScreenState> _calendarKey =
-      GlobalKey<CalendarScreenState>();
+  late final GlobalKey<GratitudeSnapScreenState> _snapKey;
+  late final GlobalKey<CalendarScreenState> _calendarKey;
+
+  late final List<TabScreen> _tabs;
+  AppTab _current = AppTab.timeline;
+
+  @override
+  void initState() {
+    super.initState();
+    _snapKey = GlobalKey<GratitudeSnapScreenState>();
+    _calendarKey = GlobalKey<CalendarScreenState>();
+
+    _tabs = [
+      TabScreen(
+        title: 'My Gratitude Journal',
+        icon: const Icon(Icons.home_outlined),
+        selectedIcon: const Icon(Icons.home),
+        build: () => const TimelineScreen(),
+      ),
+      TabScreen(
+        title: 'Gratitude Snap',
+        icon: const Icon(Icons.photo_camera_outlined),
+        selectedIcon: const Icon(Icons.photo_camera),
+        build: () => GratitudeSnapScreen(
+          key: _snapKey,
+          primaryCamera: widget.cameras.first,
+        ),
+        onEnter: () => _snapKey.currentState?.startCamera(),
+        onLeave: () => _snapKey.currentState?.stopCamera(),
+      ),
+      TabScreen(
+        title: 'Calendar',
+        icon: const Icon(Icons.calendar_today_outlined),
+        selectedIcon: const Icon(Icons.calendar_today),
+        build: () => CalendarScreen(key: _calendarKey),
+        onEnter: () => _calendarKey.currentState?.refreshCurrentMonth(),
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
-    final screens = [
-      TimelineScreen(),
-      GratitudeSnapScreen(key: _snapKey, primaryCamera: widget.cameras.first),
-      CalendarScreen(key: _calendarKey),
-    ];
-
     return Scaffold(
       appBar: _buildAppBar(),
-      body: IndexedStack(index: _current, children: screens),
+      body: IndexedStack(
+        index: _current.index,
+        children: _tabs.map((t) => t.build()).toList(),
+      ),
       bottomNavigationBar: NavigationBar(
-        selectedIndex: _current,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home_outlined),
-            selectedIcon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.photo_camera_outlined),
-            selectedIcon: Icon(Icons.photo_camera),
-            label: 'Snap',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.calendar_today_outlined),
-            selectedIcon: Icon(Icons.calendar_today),
-            label: 'Calendar',
-          ),
-        ],
-        onDestinationSelected: (i) {
-          if (i == 1 && _current != 1) {
-            // Entering snap screen and starting camera.
-            _snapKey.currentState?.startCamera();
-          } else if (i != 1 && _current == 1) {
-            if (_snapKey.currentState?.mounted ?? false) {
-              // Leaving snap screen and stopping camera.
-              _snapKey.currentState?.stopCamera();
-            }
-          } else if (i == 2) {
-            // Refreshing calendar when entering.
-            _calendarKey.currentState?.refreshCurrentMonth();
-          }
-          setState(() => _current = i);
-        },
+        selectedIndex: _current.index,
+        destinations: _tabs
+            .map(
+              (t) => NavigationDestination(
+                icon: t.icon,
+                selectedIcon: t.selectedIcon,
+                label: t.title.split(' ').first,
+              ),
+            )
+            .toList(),
+        onDestinationSelected: _onTabSelected,
       ),
     );
   }
 
+  void _onTabSelected(int newIndex) {
+    final next = AppTab.values[newIndex];
+    if (next == _current) return;
+
+    _tabs[_current.index].onLeave?.call();
+    _tabs[newIndex].onEnter?.call();
+
+    setState(() => _current = next);
+  }
+
+  /// Same streak-aware AppBar as before, but reads data from _tabs[_current].
   PreferredSizeWidget _buildAppBar() {
-    const titles = ['My Gratitude Journal', 'Gratitude Snap', 'Calendar'];
+    final currentTab = _tabs[_current.index];
 
     Widget? streakWidget;
-    if (_current == 0) {
+    if (_current == AppTab.timeline) {
       final uid = FirebaseAuth.instance.currentUser!.uid;
       streakWidget = StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
         stream: FirebaseFirestore.instance.doc('users/$uid').snapshots(),
@@ -92,12 +114,12 @@ class _MainNavState extends State<MainNav> {
     }
 
     return AppBar(
-      title: Text(titles[_current]),
+      title: Text(currentTab.title),
       actions: [
         if (streakWidget != null) streakWidget,
         PopupMenuButton<String>(
-          onSelected: (value) {
-            if (value == 'signout') showSignOutDialog(context);
+          onSelected: (v) {
+            if (v == 'signout') showSignOutDialog(context);
           },
           itemBuilder: (_) => const [
             PopupMenuItem(value: 'signout', child: Text('Sign out')),
@@ -106,4 +128,24 @@ class _MainNavState extends State<MainNav> {
       ],
     );
   }
+}
+
+class TabScreen {
+  const TabScreen({
+    required this.title,
+    required this.icon,
+    required this.selectedIcon,
+    required this.build,
+    this.onEnter,
+    this.onLeave,
+  });
+
+  final String title;
+  final Icon icon;
+  final Icon selectedIcon;
+  final Widget Function() build;
+
+  /// Optional hooks for things like starting/stopping the camera.
+  final VoidCallback? onEnter;
+  final VoidCallback? onLeave;
 }
